ace3c5e45de731b00e394da599c2a466026e468c
xShqrk <135094833+xshqrkyt@users.noreply.github.com>
2025-09-23 17:57:15 +0400

    Add files via upload

---
+++ Task4/myfirstpackage/MySecondClass.java
@@ -0,0 +1,31 @@
+package myfirstpackage;
+
+public class MySecondClass {
+    private int firstNumber;
+    private int secondNumber;
+
+    public MySecondClass(int var1, int var2) {
+        this.firstNumber = var1;
+        this.secondNumber = var2;
+    }
+
+    public int getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public void setFirstNumber(int var1) {
+        this.firstNumber = var1;
+    }
+
+    public void setSecondNumber(int var1) {
+        this.secondNumber = var1;
+    }
+
+    public int getMinimum() {
+        return this.firstNumber < this.secondNumber ? this.firstNumber : this.secondNumber;
+    }
+}
